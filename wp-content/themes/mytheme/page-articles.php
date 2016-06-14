e6d9200de1caad4a67d5d771c23edb8b475c82ae
oliegjio <oliegjio@gmail.com>
2016-06-14 15:00:26 +0300

    Commit

---
+++ wp-content/themes/mytheme/page-articles.php
@@ -0,0 +1,40 @@
+<?php
+// Этот файл описывает все страницу "Articles"
+
+// Выводит сюда содержимое файла header.php
+get_header();
+
+// Есть ли статьи
+if(have_posts()):
+  while(have_posts()):
+    // Итератор переходит на эту статью, что позволяет использовать функции
+    // снизу
+    the_post();
+
+?>
+
+<!-- Выводит блок со статьей через цикл сверху -->
+<article class="post articles-page">
+  <h1>
+    <!-- Выводит ссылку на статью -->
+    <a href="<?php the_permalink(); ?>">
+      <!-- Выводит заголовок статьи -->
+      <?php the_title(); ?>
+    </a>
+  </h1>
+  <p>
+    <!-- Выводит содержимое статьи -->
+    <?php the_content(); ?>
+  </p>
+</article>
+
+<?php
+  endwhile;
+else:
+  echo '<p>No content found</p>';
+endif;
+
+// Выводит сюда содержимое файла footer.php
+get_footer();
+
+?>
