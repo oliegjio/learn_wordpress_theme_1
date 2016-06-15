0f540f9d00044995c76429a669b444508331f68a
oliegjio <oliegjio@gmail.com>
2016-06-15 22:06:36 +0300

    Commit

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,5 +1,6 @@
 <?php
-// Этот файл описывает домашнюю страницу
+// Этот файл описывает домашнюю страницу, на которой перечисляются все
+// опубликованные посты
 
 // Выводит сюда содержимое файла header.php
 get_header();
@@ -21,6 +22,32 @@ if(have_posts()):
       <?php the_title(); ?>
     </a>
   </h2>
+  <p class="post-info">
+    <?php the_time('F jS, Y g:i a'); ?> | By
+    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+      <?php the_author(); ?>
+    </a> | Posted in
+    <?php
+    $categories = get_the_category();
+    $separator = ', ';
+    $output = '';
+
+    if($categories){
+      foreach($categories as $category){
+        $output .=
+        "<a href='" . get_category_link($category) . "'>" .
+        $category->cat_name .
+        "</a>" .
+        $separator
+        ;
+      }
+      echo trim($output, $separator);
+    }
+    ?>
+  </p>
+  <p>
+
+  </p>
   <p>
     <!-- Выводит содержимое статьи -->
     <?php the_content(); ?>
